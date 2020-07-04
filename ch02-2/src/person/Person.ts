7c507353f3cfb69ea44f7da3361de68c2d14c808
dev-chloe <chloekim.4969@gmail.com>
2020-07-04 22:13:07 +0900

    02-2 모듈 이해하기(3)-import * as 구문

--- ch02-2/src/person/Person.ts
+++ ch02-2/src/person/Person.ts
@@ -1,4 +1,4 @@
-let MAX_AGE = 100
+import * as U from '../utils/makeRandomNumber'
 
 export interface IPerson {
   name: string
@@ -9,11 +9,7 @@ class Person implements IPerson {
   constructor(public name: string, public age: number) {}
 }
 
-function makeRandomNumber(max: number = MAX_AGE): number {
-  return Math.ceil((Math.random() * max))
-}
-
 export const makePerson = (
     name: string,
-    age:number = makeRandomNumber()
+    age:number = U.makeRandomNumber()
   ) => ({ name, age })
