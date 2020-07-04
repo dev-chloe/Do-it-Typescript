9bafa213a8b92986f13bc90182fbd757d0a5806f
dev-chloe <chloekim.4969@gmail.com>
2020-07-04 22:30:39 +0900

    02-2 모듈 이해하기(5)-package import

--- ch02-2/src/index.ts
+++ ch02-2/src/index.ts
@@ -1,11 +1,20 @@
+import Chance from 'chance'
+import * as R from 'ramda'
+
 import IPerson from './person/IPerson'
-import {makePerson} from './person/Person'
+import Person, {makePerson} from './person/Person'
 // import Person, {makePerson} from './person/Person' 이렇게도 쓸 수 있음
 
-const testMakePerson = ():void => {
-  let jane: IPerson = makePerson('Jane')
-  let jack: IPerson = makePerson('Jack')
-  console.log(jane,jack)
-}
+// const testMakePerson = ():void => {
+//   let jane: IPerson = makePerson('Jane')
+//   let jack: IPerson = makePerson('Jack')
+//   console.log(jane,jack)
+// }
 
-testMakePerson()
+// testMakePerson()
+
+const chance = new Chance()
+
+let persons: IPerson[] =R.range(0,2)
+  .map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
